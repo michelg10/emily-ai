32caef5fd9403f0081af033e284841b52941f72a
Michel <michelguo@icloud.com>
2024-03-29 23:53:20 -0400

    Initial Commit

---
+++ emily-ai/emily_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  emily_aiApp.swift
+//  emily-ai
+//
+//  Created by Michel Guo on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct emily_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
